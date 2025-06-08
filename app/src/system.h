76539e47c314df01eb7f001969662482938425db
NewoPL <27411874+NewoPL@users.noreply.github.com>
2025-06-08 11:40:49 +0200

    Implement APN re-activation workaround and update system status flags for GSM

--- app/src/system.h
+++ app/src/system.h
@@ -2,12 +2,13 @@
 #define SYSTEM_H
 
 typedef enum {
-    STATUS_INITIALIZED  = 1 << 0,
-    STATUS_GPS_ON       = 1 << 1,
-    STATUS_GPS_FIX      = 1 << 2,
-    STATUS_GSM_ON       = 1 << 3,
-    STATUS_CHARGING     = 1 << 4,
-    STATUS_SLEEPING     = 1 << 5
+    STATUS_INITIALIZED    = 1 << 0,
+    STATUS_GPS_ON         = 1 << 1,
+    STATUS_GPS_FIX        = 1 << 2,
+    STATUS_GSM_ACTIVE     = 1 << 3,
+    STATUS_GSM_REGISTERED = 1 << 4,
+    STATUS_CHARGING       = 1 << 5,
+    STATUS_SLEEPING       = 1 << 6
 } StatusFlags;
 
 // Initialized
@@ -26,9 +27,9 @@ typedef enum {
 #define IS_GPS_FIX()       (systemStatus & STATUS_GPS_FIX)
 
 // GSM
-#define GSM_STATUS_ON()    (systemStatus |= STATUS_GSM_ON)
-#define GSM_STATUS_OFF()   (systemStatus &= ~STATUS_GSM_ON)
-#define IS_GSM_STATUS_ON() (systemStatus & STATUS_GSM_ON)
+#define GSM_ACTIVE_ON()    (systemStatus |= STATUS_GSM_ACTIVE)
+#define GSM_ACTIVE_OFF()   (systemStatus &= ~STATUS_GSM_ACTIVE)
+#define IS_GSM_ACTIVE()    (systemStatus & STATUS_GSM_ACTIVE)
 
 // Charging
 #define CHARGING_ON()      (systemStatus |= STATUS_CHARGING)
@@ -39,6 +40,11 @@ typedef enum {
 #define SLEEPING_ON()      (systemStatus |= STATUS_SLEEPING)
 #define SLEEPING_OFF()     (systemStatus &= ~STATUS_SLEEPING)
 #define IS_SLEEPING()      (systemStatus & STATUS_SLEEPING)
+
+// GSM Registered
+#define GSM_REGISTERED_ON()   (systemStatus |= STATUS_GSM_REGISTERED)
+#define GSM_REGISTERED_OFF()  (systemStatus &= ~STATUS_GSM_REGISTERED)
+#define IS_GSM_REGISTERED()   (systemStatus & STATUS_GSM_REGISTERED)
 
 // The actual bitfield variable
 extern uint8_t systemStatus;
